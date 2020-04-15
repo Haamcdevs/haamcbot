8f6276e08a6e5d699cb5d5d43966b9d6080a12b5
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2020-04-15 21:35:41 +0200

    Working version

--- cogs/cots.py
+++ cogs/cots.py
@@ -6,13 +6,14 @@ class Cots(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @commands.command(pass_context=True, name='cots start')
+    @commands.command(pass_context=True, name='cots_start')
     @commands.has_role(config.role_global_mod)
-    async def start(self, ctx, *, member):
-        channel = next(ch for ch in member.guild.channels if ch.id == config.cots_channel)
-        role = next(r for r in member.guild.roles if r.id == config.role_user)
+    async def start(self, ctx):
+        user = ctx.message.author
+        channel = next(ch for ch in user.guild.channels if ch.id == config.cots_channel)
+        role = next(r for r in user.guild.roles if r.id == config.role_user)
         await channel.send(f"Bij deze zijn de nominaties voor season {config.cots_season} geopend!")
-        await channel.set_permissions(role, send_messages=True)
+        await channel.set_permissions(role, send_messages=True, reason=f'Starting cots, triggered by {user.name}')
 
 
 def setup(bot):
