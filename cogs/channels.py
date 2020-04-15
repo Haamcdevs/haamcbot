ab8ac699db3dbb4e23ff6fcae9f4bd4c18aa0ba0
XSlicer <xslicer@xslicer.nl>
2020-04-15 17:42:31 +0200

    Adding message to joinable channels (getting data from MAL)

--- cogs/channels.py
+++ cogs/channels.py
@@ -1,13 +1,41 @@
 import discord
+import re
 from discord.ext import commands
+from jikanpy import Jikan
+
+jikan = Jikan()
 
 
 class Channels(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    def _joinmessage(self, ctx, channel, category):
-        pass
+    @staticmethod
+    def _getmaldata(url):
+        # Using regex to get MAL id out of URL (easier to use with Jikan)
+        mal_id = re.search('\d+', url)
+        anime = jikan.anime(mal_id[0])
+        return anime
+
+    @staticmethod
+    async def _joinmessage(channel, categorychannel, malurl):
+        maldata = Channels._getmaldata(malurl)
+        embed = discord.Embed(
+            title=maldata['title'],
+            type='rich',
+            url=malurl
+        )
+        # embed.set_author(name=maldata['title'], icon_url='', url=malurl)
+        embed.set_footer(text='Druk op de reactions om te joinen / leaven')
+        embed.set_thumbnail(url=maldata['image_url'])
+        embed.add_field(name='studio', value=', '.join([stu['name'] for stu in maldata['studios']]))
+        embed.add_field(name='datum', value=maldata['aired']['string'])
+        embed.add_field(name='genres', value=', '.join([gen['name'] for gen in maldata['genres']]))
+        embed.add_field(name='channel', value=channel.mention)
+        embed.add_field(name='kijkers', value=len(channel.members)-1)
+        msg = await categorychannel.send(embed=embed)
+        await msg.add_reaction('▶')
+        await msg.add_reaction('⏹')
 
     @commands.command(pass_context=True)
     @commands.has_any_role("Shinsengumi", "Shinobi")
@@ -28,6 +56,12 @@ class Channels(commands.Cog):
                 guild.me: discord.PermissionOverwrite(read_messages=True)
             }
         )
+        for cat in guild.categories:
+            if 'joinable-channels' in cat.name:
+                for chan in cat.channels:
+                    if 'anime' in chan.name:
+                        categorychannel = chan
+        await self._joinmessage(newchan, categorychannel, malurl)
         await ctx.message.delete()
         await newchan.send(f"Hallo iedereen! In deze channel kijken we naar **{title}**.\n{malurl}")
 
