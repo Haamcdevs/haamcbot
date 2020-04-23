1dac83de578bb64244065a3bab06b5d82fe43a8c
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2020-04-23 22:00:38 +0200

    ranking command

--- cogs/bikkelpunt.py
+++ cogs/bikkelpunt.py
@@ -13,7 +13,7 @@ database = mysql.connector.connect(
 
 class Bikkelpunt_utils(object):
     def __init__(self):
-        self.bikkelpunt_cursor = database.cursor()
+        self.bikkelpunt_cursor = database.cursor(dictionary=True)
 
     def is_time_correct(self):
         # If check time is not given, default to current UTC time
@@ -36,15 +36,32 @@ class Bikkelpunt_utils(object):
             return True
         return False
 
+    def load_top_10(self):
+        sql = 'SELECT * FROM bikkel ORDER BY points DESC LIMIT 0,10'
+        self.bikkelpunt_cursor.execute(sql)
+        return self.bikkelpunt_cursor.fetchall()
+
+    def get_top_10_message(self):
+        msg = ":last_quarter_moon_with_face: **HAAMC Discord bikkel ranking** *top 10*\n"
+        for bikkel in self.load_top_10():
+            print(bikkel)
+            msg += f"{bikkel['display_name']}\n"
+        return msg
+
+
 class Bikkelpunt(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.utils = Bikkelpunt_utils()
     
-    @commands.group(name='bikkelpunt', invoke_without_commands=False)
-    async def bikkelpunt(self, ctx):
-        utils = Bikkelpunt_utils()
-        check_time = utils.is_time_correct()
-        has_cooldown = utils.has_cooldown(ctx.message.author.id)
+    @commands.group(name='bikkel', invoke_without_commands=False, help='Bikkel punten')
+    async def bikkel(self, ctx):
+        return
+
+    @bikkel.command(pass_context=True, help='Verdien een bikkelpunt')
+    async def get(self, ctx):
+        check_time = self.utils.is_time_correct()
+        has_cooldown = self.utils.has_cooldown(ctx.message.author.id)
         if check_time is False:
             await ctx.channel.send(
                 f":last_quarter_moon_with_face: Echte bikkels leven tussen 03:00 en 05:00!"
@@ -58,5 +75,11 @@ class Bikkelpunt(commands.Cog):
         await ctx.channel.send(
             f":last_quarter_moon_with_face: Je bent een echte bikkel! **+1** (**%s** punten totaal)"
             )
+
+    @bikkel.command()
+    async def ranking(self, ctx):
+        await ctx.channel.send(self.utils.get_top_10_message())
+
+
 def setup(bot):
     bot.add_cog(Bikkelpunt(bot))
