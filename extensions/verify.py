29869a134bbb60720f99c7fe26c572b2515b01fe
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2022-12-14 21:39:51 +0100

    Add verify command

---
+++ extensions/verify.py
@@ -0,0 +1,29 @@
+import re
+
+from discord.ext import commands
+from discord.ext.commands import Context
+
+import config
+
+
+@commands.hybrid_command(help='Mods only')
+@commands.has_role(config.role['global_mod'])
+async def verify(ctx: Context, user: str):
+    user = re.findall(r"\d+", user) or None
+    if user is None:
+        await ctx.send(':x: Invalid user mention or id', ephemeral=True);
+    user = int(user[0])
+    user = ctx.guild.get_member(user)
+    role = ctx.guild.get_role(config.role['user'])
+    await user.add_roles(role, reason=f'User verified by {ctx.author.name}')
+    channel = ctx.guild.get_channel_or_thread(config.channel['general'])
+    await ctx.send(f'Verified member {user.mention}', ephemeral=True)
+    emoji = [emote for emote in ctx.guild.emojis if emote.name == 'pikawave'][0]
+    await channel.send(
+        f'Welkom {user.mention}! <a:{emoji.name}:{emoji.id}>\n'
+        f'Kijk in de <#513719588165386241> voor meer opties.'
+    )
+
+
+async def setup(bot):
+    bot.add_command(verify)
