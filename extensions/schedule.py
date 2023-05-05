4dad2b03348dd99bc0667da521de95bf6395cf93
Jan Vernieuwe <jan.vernieuwe@phpro.be>
2023-05-05 15:25:44 +0200

    Revert "Make anime schedule personal"
    This reverts commit a4d607f79784eee7cbbcfac46961fecb8a23ecd1.

--- extensions/schedule.py
+++ extensions/schedule.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-
-from discord.errors import NotFound
 from discord.ext import commands
 from discord.ext.commands import Context
 
@@ -11,20 +8,14 @@ from util.airing import Airing
 @commands.hybrid_command(help='Show anime schedule', category='Anime')
 async def schedule(ctx: Context):
     airing = Airing()
-    messages = ['Jou animerooster voor komende week']
+    messages = ['Animerooster voor komende week']
     upcoming_episodes = airing.load_upcoming(24*7)
     if len(upcoming_episodes) == 0:
         await ctx.send('No episodes found in next week', ephemeral=True)
         return
-    member = ctx.guild.get_member(ctx.author.id)
     for episode in upcoming_episodes:
-        post = ctx.guild.get_channel_or_thread(episode["channel_id"])
-        try:
-            await post.fetch_member(ctx.author.id)
-            messages.append(f'<#{episode["channel_id"]}> episode **{episode["episode"]}** <t:{episode["airing"]}:R>')
-        except NotFound:
-            continue
-    await ctx.send("\n".join(messages), ephemeral=True)
+        messages.append(f'<#{episode["channel_id"]}> episode **{episode["episode"]}** <t:{episode["airing"]}:R>')
+    await ctx.send("\n".join(messages))
 
 
 async def setup(bot):
